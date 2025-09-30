6fcf96aeeebb0a1304ed32c5df5a2981f2f35be3
Marcos Calitas <mcalitas@isqapave.com>
2025-09-30 11:33:47 +0100

    feat: add new methods

--- app/Http/Controllers/Admin/Auth/PasswordController.php
+++ app/Http/Controllers/Admin/Auth/PasswordController.php
@@ -5,23 +5,66 @@ namespace App\Http\Controllers\Admin\Auth;
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class PasswordController extends Controller
 {
     private const ADMIN_AUTH_PASSWORD = 'admin.auth.password.';
 
-    public function forgot()
+    public function showForgotForm()
     {
         return view(self::ADMIN_AUTH_PASSWORD . 'forgot');
     }
 
-    public function checkEmail()
+    public function sendResetLink(Request $request)
     {
-        return view(self::ADMIN_AUTH_PASSWORD . 'check-email');
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? redirect()->route('admin.check-email')->with(['email' => $request->get("email")])
+            : back()->withErrors(['email' => __($status)]);
     }
 
-    public function reset()
+    public function showCheckEmail(Request $request)
     {
-        return view(self::ADMIN_AUTH_PASSWORD . 'reset');
+        $email = session('email');
+        return view(self::ADMIN_AUTH_PASSWORD . 'check-email', compact('email'));
+    }
+
+    public function showResetForm(Request $request, $token)
+    {
+        return view(self::ADMIN_AUTH_PASSWORD . 'reset', [
+            'token' => $token,
+            'email' => $request->email
+        ]);
+    }
+
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+            ? redirect()->route('admin.login')->with('success', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
     }
 }
