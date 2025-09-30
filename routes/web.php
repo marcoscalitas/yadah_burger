f0247da363555940823b2aef22c38f4ec48a165e
Marcos Calitas <mcalitas@isqapave.com>
2025-09-30 15:44:55 +0100

    feat: add eror 404 handler

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 // Create admin routes file
-Route::prefix('admin')->name('admin.')->group(base_path('routes/admin.php'));
+Route::prefix('admin')->name('admin.')->group(function () {
+    require base_path('routes/admin.php');
+});
 
-// Fallback for admin routes
-// Route::fallback(function () {
-//     return dd("404 ERROR");
-// });
+Route::fallback(function () {
+    return response()->view('shared.errors.error-404', [], 404);
+});
