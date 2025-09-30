16f184ae597a234e017161ab2b60a8cd4fd28cd4
Marcos Calitas <mcalitas@isqapave.com>
2025-09-30 11:24:37 +0100

    refactor: add route name 'admin'

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 // Create admin routes file
-Route::prefix('admin')->group(base_path('routes/admin.php'));
+Route::prefix('admin')->name('admin.')->group(base_path('routes/admin.php'));
+
+// Fallback for admin routes
+// Route::fallback(function () {
+//     return dd("404 ERROR");
+// });
