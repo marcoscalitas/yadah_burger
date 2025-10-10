ef784cbf6a12badce9a910540f7199592f761794
Marcos Calitas <mcalitas@isqapave.com>
2025-10-10 10:44:45 +0100

    fix: call getStatusBadge helper

--- resources/views/admin/dash/users/show.blade.php
+++ resources/views/admin/dash/users/show.blade.php
@@ -48,7 +48,7 @@
                                 <small class="text-muted">Sexo</small>
                             </div>
                             <div class="col-span-4">
-                                {!! $user->getStatusBadge() !!}
+                                {!! getStatusBadge($user->user_status) !!}
                                 <small class="text-muted">Status</small>
                             </div>
                         </div>
