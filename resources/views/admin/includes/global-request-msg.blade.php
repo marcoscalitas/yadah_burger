ef27eae8ea0b8330386500b1579e00a54d5c6947
Marcos Calitas <marcoscalitas@gmail.com>
2025-10-16 11:40:37 +0100

    feat: add messages type

--- resources/views/admin/includes/global-request-msg.blade.php
+++ resources/views/admin/includes/global-request-msg.blade.php
@@ -1,4 +1,3 @@
-<!-- [ Messages ] start -->
 @if (session()->has('success'))
     <div class="alert alert-success message-fade-out">
         <span>
@@ -7,14 +6,34 @@
         {{ session('success') }}
     </div>
 @endif
-{{-- @if (session()->has('error'))
-                <div class="alert alert-danger message-fade-out">
-                    <span>
-                        <i class="fas fa-exclamation-circle fa-lg me-2"></i>
-                    </span>
-                    {{ session('error') }}
-                </div>
-            @endif --}}
+
+@if (session()->has('info'))
+    <div class="alert alert-info message-fade-out">
+        <span>
+            <i class="fas fa-info-circle fa-lg me-2"></i>
+        </span>
+        {{ session('info') }}
+    </div>
+@endif
+
+@if (session()->has('error'))
+    <div class="alert alert-danger message-fade-out">
+        <span>
+            <i class="fas fa-exclamation-circle fa-lg me-2"></i>
+        </span>
+        {{ session('error') }}
+    </div>
+@endif
+
+@if (session()->has('warning'))
+    <div class="alert alert-warning message-fade-out">
+        <span>
+            <i class="fas fa-exclamation-triangle fa-lg me-2"></i>
+        </span>
+        {{ session('warning') }}
+    </div>
+@endif
+
 @if ($errors->any() && !$errors->has('error'))
     <div class="alert alert-danger message-fade-out">
         <span>
@@ -28,4 +47,3 @@
         </ul>
     </div>
 @endif
-<!-- [ Messages ] end -->
